6bb0e7e95b08e1ea680cedfa2d7035f276f6c4be
Boring Wozniak <52351871+boring-wozniak@users.noreply.github.com>
2022-04-30 15:08:50 +0200

    Fix build

--- {{ cookiecutter.project }}/app/src/main/java/{{ cookiecutter.package }}/Application.java
+++ {{ cookiecutter.project }}/app/src/main/java/{{ cookiecutter.package }}/Application.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public final class Application {
 
     public static void main(String[] arguments) {
-        LOG.info("Yo!");
+        log.info("Yo!");
     }
 
 }
