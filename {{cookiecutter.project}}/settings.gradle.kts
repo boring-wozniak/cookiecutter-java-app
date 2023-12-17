49402ed3f0de06ef2b2f407981ed0a7ca51725f4
Boring Wozniak <52351871+boring-wozniak@users.noreply.github.com>
2023-12-17 12:45:15 +0100

    Publish Gradle Build reports

--- {{cookiecutter.project}}/settings.gradle.kts
+++ {{cookiecutter.project}}/settings.gradle.kts
@@ -1,5 +1,18 @@
+import java.lang.System.getenv as env
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "latest.release"
+    id("com.gradle.enterprise") version "latest.release"
 }
 
 rootProject.name = "{{cookiecutter.project}}"
+
+gradleEnterprise {
+    buildScan {
+        publishAlwaysIf(isRunningOnCi())
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
+fun isRunningOnCi() = env("CI") == "true"
